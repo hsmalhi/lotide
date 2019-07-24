c883af624c8411e2faab1094fd179b69a592b373
Harjot Singh Malhi <hsmalhi94@hotmail.com>
2019-07-24 16:32:47 +0000

    Completed countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -1,6 +1,29 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected)
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  else
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
+
+// str: the string that we need to look through
+const countLetters = function(str) {
+  const results = {};
+  str = str.toLowerCase();
+
+  for (const letter of str) {
+    if (!results[letter])
+      results[letter] = 1;
+    else
+      results[letter]++;
+  }
+
+  return results;
+};
+
+/*
+//Test code
+const result1 = countLetters("lighthouse in the house");
+
+assertEqual(result1["l"], 1);
+assertEqual(result1["i"], 2);
+assertEqual(result1["e"], 3);
+*/
