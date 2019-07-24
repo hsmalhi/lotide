751b257cfc1d329a3263f4ef159c6388b81ce04f
Harjot Singh Malhi <hsmalhi94@hotmail.com>
2019-07-24 18:13:19 +0000

    Completed eqObjects.js for primitive values

--- eqObjects.js
+++ eqObjects.js
@@ -5,16 +5,45 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length)
+    return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i])
+      return false;
+  }
+  return true;
+};
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  if (eqObjectsLeftInsideRight(object1, object2))
+    return eqObjectsLeftInsideRight(object2, object1)
+  return false
+};
 
+const eqObjectsLeftInsideRight = function(leftObject, rightObject){
+  for(key in leftObject){
+    if (leftObject[key] !== rightObject[key])
+      return false
+  }
+  return true;
 };
 
 //Test cases
+//Primitives as values
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false); // => false
+
+//Arrays as values
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
