215f49cd65e08a593df65cf9bea16c6e7b086e60
Harjot Singh Malhi <hsmalhi94@hotmail.com>
2019-07-26 15:57:24 +0000

    Implemented functionality to compare nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -9,13 +9,24 @@ const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length)
     return false;
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i])
+    if (Array.isArray(arr1[i])) {
+      if (!(eqArrays(arr1[i], arr2[i]))){
+        return false;
+      }
+    }
+    else if (arr1[i] !== arr2[i])
       return false;
   }
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+//Test code
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+// assertEqual(eqArrays([[2, 3, [6, [6, 7, [1, 6, 7, 8]]]], [4]], [[2, 3, [6, [6, 7, [1, 6, 7, 8]]]], [4]]), true); // => false
+// assertEqual(eqArrays([[2, 3, [6, [6, 7, [1, 6, 7, 8]]]], [4]], [[2, 3, [6, [6, 7, [1, 6, 9, 8]]]], [4]]), false); // => false
